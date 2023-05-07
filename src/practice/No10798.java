6dec07e732cfd7e5a66e3ea8c410d2717449a526
윤혁준(dev_y) <gurwns1540@gmail.com>
2023-05-07 22:45:58 +0900

    문제 추가

--- src/practice/No10798.java
+++ src/practice/No10798.java
@@ -9,6 +9,22 @@ public class No10798 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String[][] list = new String[5][15];
+        //input data
+        for (int i = 0; i < 5; i++) {
+            String[] s = br.readLine().split("");
+            String[] tmp = Arrays.copyOf(s, 15);
+            list[i] = tmp;
+        }
+
+        //process
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(Objects.nonNull(list[j][i])) {
+                    bw.write(list[j][i]);
+                }
+            }
+        }
 
 
         bw.flush();
---
+++ src/practice/No2563.java
@@ -0,0 +1,34 @@
+package practice;
+
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
+public class No2563 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] result = new int[100][100];
+
+        for (int i = 0; i < n; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int a = input[0];
+            int b = input[1];
+            for (int x = a; x < a + 10; x++) {
+                for (int y = b; y < b + 10; y++) {
+                    result[x][y] = 1;
+                }
+            }
+        }
+
+        long count = Arrays.stream(result).flatMapToInt(Arrays::stream).filter(v -> v == 1).count();
+
+        bw.write(String.valueOf(count));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
