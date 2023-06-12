7e064d09de9aa2f02886040028cf6b76ebd49209
윤혁준(dev_y) <gurwns1540@gmail.com>
2023-06-12 09:29:58 +0900

    [add] 문제풀이 추가 및 진행중   1. 24313 진행중

--- src/practice/from24000to24999/No24267.java
+++ src/practice/from24000to24999/No24267.java
@@ -5,15 +5,15 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class No24265 {
+public class No24267 {
     public static void main(String[] args) throws Exception{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         long n = Integer.parseInt(br.readLine());
 
-        bw.write(String.format("%s\n", n * (n - 1) / 2));
-        bw.write(String.valueOf(2));
+        bw.write(String.format("%s\n", n * (n - 1) * (n - 2) / 6));
+        bw.write(String.valueOf(3));
 
         bw.flush();
         bw.close();
---
+++ src/practice/from24000to24999/No24313.java
@@ -0,0 +1,26 @@
+package practice.from24000to24999;
+import java.math.*;
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class No24313 {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s = br.readLine().split(" ");
+
+        int a1 = Integer.parseInt(s[0]);
+        int a0 = Integer.parseInt(s[1]);
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
