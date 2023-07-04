9de2a46d8f3950c60e0fc47d68f46a50ce3f0ee7
윤혁준(dev_y) <gurwns1540@gmail.com>
2023-07-04 21:45:49 +0900

    [add] 문제풀이 추가 및 진행중   1. 2798 풀이중

--- src/practice/from2000to2999/No2798.java
+++ src/practice/from2000to2999/No2798.java
@@ -14,17 +14,27 @@ public class No2798 {
         //합계
         int m = Integer.parseInt(s[1]);
 
-        int[] ilist = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] ilist = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        System.out.println(Arrays.toString(ilist));
         int result = 0;
 
+        end:
         for (int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
                 for(int k = j + 1; k < n; k++) {
-
+                    int tmp = ilist[i] + ilist[j] + ilist[k];
+                    if(tmp == m) {
+                        result = tmp;
+                        break end;
+                    }
+                    if(tmp >= result && tmp < m) {
+                        result = tmp;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
-
 
         bw.write(String.valueOf(result));
 
