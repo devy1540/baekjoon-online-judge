3f335b4e16511a607964913e0c1693e81cc35ee6
윤혁준(dev_y) <gurwns1540@gmail.com>
2023-07-04 09:28:57 +0900

    [add] 문제풀이 추가 및 진행중   1. 2798 풀이중

--- src/practice/from2000to2999/No2798.java
+++ src/practice/from2000to2999/No2798.java
@@ -8,7 +8,25 @@ public class No2798 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] s = br.readLine().split(" ");
+        //카드 갯수
+        int n = Integer.parseInt(s[0]);
+        //합계
+        int m = Integer.parseInt(s[1]);
 
+        int[] ilist = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                for(int k = j + 1; k < n; k++) {
+
+                }
+            }
+        }
+
+
+        bw.write(String.valueOf(result));
 
         bw.flush();
         bw.close();
