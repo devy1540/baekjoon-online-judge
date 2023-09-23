4abf9b86c22a6401eed102cec2b0bd8ed3239889
윤혁준(dev_y) <gurwns1540@gmail.com>
2023-09-23 22:51:59 +0900

    [add] 문제풀이 추가 및 진행중   1. 1018 풀이 중

--- src/practice/from1000to1999/No1018.java
+++ src/practice/from1000to1999/No1018.java
@@ -13,12 +13,64 @@ public class No1018 {
         String[] s = br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
         int m = Integer.parseInt(s[1]);
+        int min = Integer.MAX_VALUE;
 
-        boolean[][] arr = new boolean[n][m];
+//        boolean[][] sWhite = makeInitBoard(true);
+//        boolean[][] sBlack = makeInitBoard(false);
 
+        String[][] arr = new String[n][m];
+
+        for(int i = 0; i < n; i++) {
+            s = br.readLine().split("");
+            System.arraycopy(s, 0, arr[i], 0, m);
+        }
+
+        System.out.println(Arrays.deepToString(makeInitBoard(true)));
+
+//        for(int i = 0; i <= n - 8; i++) {
+//            for(int j = 0; j <= m - 8; j++) {
+//                min = Math.min(min, cal(i, j, arr));
+//            }
+//        }
+
+        bw.write(String.valueOf(min));
 
         bw.flush();
         bw.close();
         br.close();
     }
+
+    //true는 white로 시작, false는 black으로 시작
+    public static String[][] makeInitBoard(boolean color) {
+        String[][] board = new String[8][8];
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(color) {
+                    board[i][j] = "W";
+                } else {
+                    board[i][j] = "B";
+                }
+                color = !color;
+            }
+            color = !color;
+        }
+        return board;
+    }
+
+//    public static int cal(int x, int y, String[][] board) {
+//        int n = 0;
+////        boolean init = board[x][y];
+//        boolean[][] init = makeInitBoard(board[x][y]);
+//
+//        for(int i = 0; i < 8; i++) {
+//            for(int j = y; j < 8; j++) {
+//                if(board[x + i][y + j] != init[i][j]) {
+//                    n++;
+//                }
+//            }
+//        }
+//
+//        return n;
+//    }
 }
