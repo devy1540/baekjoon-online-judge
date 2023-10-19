a0f50899a36c6491b708346f72364c61113c893d
윤혁준(dev_y) <gurwns1540@gmail.com>
2023-10-19 22:43:46 +0900

    [add] 문제풀이 진행중

--- src/practice/from1000to1999/No1966.java
+++ src/practice/from1000to1999/No1966.java
@@ -11,33 +11,56 @@ public class No1966 {
         StringBuilder sb = new StringBuilder();
 
         int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++) {
-            Queue<int[]> queue = new LinkedList<>();
+//        for(int i = 0; i < t; i++) {
+        while(t --> 0) {
+            Queue<int[]> list = new LinkedList<>();
             String[] s = br.readLine().split(" ");
+            //문서의 갯수
             int n = Integer.parseInt(s[0]);
+
+            //구하고자 하는 문서의 번호가 몇번째 queue에 있는지(위치)
             int m = Integer.parseInt(s[1]);
+
+            //문서 중요도
             int[] priority = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+            //리스트에 문서 저장
             for(int j = 0; j < n; j++) {
-                queue.add(new int[]{j, priority[j]});
+                list.offer(new int[]{j, priority[j]});
             }
-            int result = 0;
 
-            while(true) {
-                int[] tmp = queue.poll();
-                if(queue.stream().anyMatch(il -> tmp[1] <= il[1])) {
-                    queue.offer(tmp);
-                    int[] tmp2 = queue.stream().filter(il -> tmp[1] <= il[1]).findFirst().orElseThrow();
-                    if(tmp2[0] == m) {
-                        sb.append(result);
+
+            //출력횟수
+            int count = 0;
+
+            while(!list.isEmpty()) {
+                int[] front = list.poll();
+                boolean b = true;
+
+                for(int i = 0; i < list.size(); i++) {
+                    if(front[1] < list.peek()[1]) {
+                        list.offer(front);
+
+                        for(int j = 0; j < i; j++) {
+                            list.offer(list.poll());
+                        }
+
+                        b = false;
                         break;
-                    } else {
-                        queue.remove(tmp2);
-                        result++;
                     }
                 }
+
+                if(!b) {
+                    continue;
+                }
+
+                count++;
+                if(front[0] == m) {
+                    break;
+                }
             }
-            sb.append("\n");
+
+            sb.append(count).append("\n");
         }
 
         bw.write(sb.toString());
